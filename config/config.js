7e31689e7616b908ab3997f605ceae59ff9b8c95
premieredesign <premieredesign@icloud.com>
2017-07-25 19:19:07 -0400

    Angular 2 Project commit and upload

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     dev: {
-        port: process.env.port || 9000,
+        port: process.env.PORT || 9000,
         db: process.env.DB_Link || 'mongodb://cdaowner:M4Di5on*@ds115071.mlab.com:15071/cdarest'
     },
     prod: {
